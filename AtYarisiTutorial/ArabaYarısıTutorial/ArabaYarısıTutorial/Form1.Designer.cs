93aa9dc12dd8048003540a67ad80193f78dcea0a
Uğur <77697813+ugurs061@users.noreply.github.com>
2022-03-31 16:38:12 +0300

    At Yarısı

---
+++ AtYarisiTutorial/ArabaYarısıTutorial/ArabaYarısıTutorial/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ArabaYarısıTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxKirmiziAt = new System.Windows.Forms.PictureBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxPembeAt = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMaviAt = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKirmiziAt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPembeAt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMaviAt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxKirmiziAt
+            // 
+            this.pictureBoxKirmiziAt.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxKirmiziAt.Image")));
+            this.pictureBoxKirmiziAt.Location = new System.Drawing.Point(12, 35);
+            this.pictureBoxKirmiziAt.Name = "pictureBoxKirmiziAt";
+            this.pictureBoxKirmiziAt.Size = new System.Drawing.Size(103, 95);
+            this.pictureBoxKirmiziAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxKirmiziAt.TabIndex = 0;
+            this.pictureBoxKirmiziAt.TabStop = false;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(4, 376);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(118, 43);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Oyna";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(1, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1246, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBoxPembeAt
+            // 
+            this.pictureBoxPembeAt.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPembeAt.Image")));
+            this.pictureBoxPembeAt.Location = new System.Drawing.Point(12, 136);
+            this.pictureBoxPembeAt.Name = "pictureBoxPembeAt";
+            this.pictureBoxPembeAt.Size = new System.Drawing.Size(103, 87);
+            this.pictureBoxPembeAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPembeAt.TabIndex = 3;
+            this.pictureBoxPembeAt.TabStop = false;
+            // 
+            // pictureBoxMaviAt
+            // 
+            this.pictureBoxMaviAt.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMaviAt.Image")));
+            this.pictureBoxMaviAt.Location = new System.Drawing.Point(15, 227);
+            this.pictureBoxMaviAt.Name = "pictureBoxMaviAt";
+            this.pictureBoxMaviAt.Size = new System.Drawing.Size(100, 91);
+            this.pictureBoxMaviAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMaviAt.TabIndex = 4;
+            this.pictureBoxMaviAt.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(1, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1246, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(1, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1246, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(1, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1247, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.BackColor = System.Drawing.Color.Black;
+            this.lblFinish.Location = new System.Drawing.Point(1253, 5);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(42, 319);
+            this.lblFinish.TabIndex = 8;
+            this.lblFinish.Text = "label5";
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.Location = new System.Drawing.Point(147, 376);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(132, 43);
+            this.btnYeniOyun.TabIndex = 9;
+            this.btnYeniOyun.Text = "Yeni Oyun";
+            this.btnYeniOyun.UseVisualStyleBackColor = true;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(364, 380);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(471, 43);
+            this.lblDurum.TabIndex = 10;
+            this.lblDurum.Text = "Durum";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1467, 472);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBoxMaviAt);
+            this.Controls.Add(this.pictureBoxPembeAt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.pictureBoxKirmiziAt);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "At Yarışı by Uğur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKirmiziAt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPembeAt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMaviAt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxKirmiziAt;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBoxPembeAt;
+        private System.Windows.Forms.PictureBox pictureBoxMaviAt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.Button btnYeniOyun;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ AtYarisiTutorial/ArabaYarısıTutorial/ArabaYarısıTutorial/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArabaYarısıTutorial
+{
+    public partial class Form1 : Form
+    {
+        //Ses ekleme
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Users\rexts\Desktop\AtYarisiTutorial\ArabaYarısıTutorial\ArabaYarısıTutorial\bin\Debug\ses\at.wav");
+        System.Media.SoundPlayer finishSoundPlayer = new System.Media.SoundPlayer(@"C:\Windows\Media\tada.wav");
+
+        Random r = new Random();
+        int x, y, z;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+         //finish 
+            if (pictureBoxKirmiziAt.Location.X - pictureBoxKirmiziAt.Width >= lblFinish.Location.X-130)
+            {
+                timer1.Stop();
+                lblDurum.Text = "BEYAZ AT KAZANDI";
+                this.BackColor = System.Drawing.Color.White;
+                MessageBox.Show("BEYAZ AT KAZANDI");
+                finishSoundPlayer.Play();//bitiş sesi
+                btnYeniOyun.Enabled = true;
+                return;
+
+            }
+            if (pictureBoxPembeAt.Location.X - pictureBoxPembeAt.Width >= lblFinish.Location.X-130)
+            {
+                timer1.Stop();
+                lblDurum.Text = "PEMBE AT KAZANDI";
+                this.BackColor = System.Drawing.Color.Pink;
+                MessageBox.Show("PEMBE AT KAZANDI");
+                finishSoundPlayer.Play();//bitiş sesi
+                btnYeniOyun.Enabled = true;
+                return;
+
+            }
+            if (pictureBoxMaviAt.Location.X - pictureBoxMaviAt.Width >= lblFinish.Location.X-130)
+            {
+                timer1.Stop();
+                lblDurum.Text = "MAVİ AT KAZANDI";
+                this.BackColor = System.Drawing.Color.Blue;
+                MessageBox.Show("MAVİ AT KAZANDI");
+                finishSoundPlayer.Play();//bitiş sesi
+                btnYeniOyun.Enabled = true;
+                return;
+
+            }
+            //hareket
+            x = r.Next(10, 30);
+            pictureBoxKirmiziAt.Left += x;
+            y = r.Next(10, 30);
+            pictureBoxPembeAt.Left += y;
+            z = r.Next(10, 30);
+            pictureBoxMaviAt.Left += z;
+
+            //durum info
+            if (pictureBoxKirmiziAt.Location.X > pictureBoxPembeAt.Location.X && pictureBoxKirmiziAt.Location.X > pictureBoxMaviAt.Location.X)
+            {
+                lblDurum.Text = "Beyaz at önde";
+                lblDurum.ForeColor = Color.White;
+              
+            }
+            else if (pictureBoxPembeAt.Location.X > pictureBoxKirmiziAt.Location.X && pictureBoxPembeAt.Location.X > pictureBoxMaviAt.Location.X)
+            {
+                lblDurum.Text = "Pembe at önde";
+                lblDurum.ForeColor = Color.Pink;
+
+            }
+            else if (pictureBoxMaviAt.Location.X > pictureBoxKirmiziAt.Location.X && pictureBoxMaviAt.Location.X > pictureBoxPembeAt.Location.X)
+            {
+                lblDurum.Text = "Mavi at önde";
+                lblDurum.ForeColor = Color.Blue;
+
+            }
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            //Yeni oyun atları başlangıç konumuna getirme 
+            this.BackColor = System.Drawing.Color.Green;
+
+            pictureBoxKirmiziAt.Left = 1;
+            pictureBoxPembeAt.Left = 1;
+            pictureBoxMaviAt.Left = 1;
+
+            lblDurum.Text = "";
+            btnYeniOyun.Enabled = false;
+
+            startSoundPlayer.Play();
+
+        }
+
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // adım adım hareket etmek için timer2 kullandık
+            timer1.Stop();
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           //space basınca çalışması için
+            if (e.KeyCode == Keys.Space)
+            {
+                
+                timer1.Start();
+                timer2.Start();
+            }
+            
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startSoundPlayer.Play();
+        }
+
+        
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+            timer2.Start();
+            
+            
+            btnYeniOyun.Enabled = false;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
